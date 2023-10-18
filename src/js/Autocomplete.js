1ffbf7dba059dc5c030f0f846f27f4d46a806ee9
Matheus Telles <business.mwtelles@gmail.com>
2023-10-18 15:30:38 -0300

    Ajustando estrutura

--- src/js/Autocomplete.js
+++ src/js/Autocomplete.js
@@ -13,11 +13,14 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function updateSuggestions(suggestions) {
+    const searchContainer = document.getElementById("search-container"); 
     suggestionsPanel.innerHTML = "";
     if (suggestions.length > 0) {
-      suggestionsPanel.classList.add("visible");
+        suggestionsPanel.classList.add("visible");
+        searchContainer.classList.add("no-radius");
     } else {
-      suggestionsPanel.classList.remove("visible");
+        suggestionsPanel.classList.remove("visible");
+        searchContainer.classList.remove("no-radius");
     }
     suggestions.forEach((suggestion) => {
       const suggestionElement = document.createElement("li");
@@ -44,4 +47,37 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 300);
 
   inputField.addEventListener("input", onInput);
+
+  function loadWords(event) {
+    const file = event.target.files[0];
+
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (e) {
+        const content = e.target.result;
+        try {
+          const words = JSON.parse(content);
+          words.forEach((word) => {
+            trie.insert(word);
+          });
+          alert("Palavras carregadas com sucesso!");
+        } catch (error) {
+          alert("Falha ao analisar o arquivo JSON.");
+        }
+      };
+      reader.readAsText(file);
+    } else {
+      alert("Nenhum arquivo selecionado.");
+    }
+  }
+
+  document
+    .getElementById("load-words-button")
+    .addEventListener("click", function () {
+      document.getElementById("wordlist-input").click();
+    });
+
+  document
+    .getElementById("wordlist-input")
+    .addEventListener("change", loadWords);
 });
